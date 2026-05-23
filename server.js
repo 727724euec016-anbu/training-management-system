fe8fc2e9afc305036b9c1d102e886f9c61e22200
Anbarasu S <anbumakhilavan@gmail.com>
2026-05-23 12:02:07 +0530

    Fixed Render deployment

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const Session = require('./models/Session');
-const Participant =
-require('./models/Participant');
+const Participant = require('./models/Participant');
 
 const app = express();
 
@@ -12,51 +11,45 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb+srv://727724euec016_db_anbu:dbanbu123@cluster0.lbd1lor.mongodb.net/?appName=Cluster0')
-.then(() => console.log('MongoDB Connected'))
+mongoose.connect(
+  'mongodb+srv://727724euec016_db_anbu:dbanbu123@cluster0.lbd1lor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+)
+.then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('Training Management Server Running');
+  res.send('Training Management Server Running');
 });
+
 app.get('/sessions', async (req, res) => {
-
-    const sessions = await Session.find();
-
-    res.json(sessions);
-
+  const sessions = await Session.find();
+  res.json(sessions);
 });
-app.post('/add-participant',
-async (req, res) => {
 
-    const newParticipant =
-    new Participant(req.body);
+app.post('/add-session', async (req, res) => {
+  const newSession = new Session(req.body);
 
-    await newParticipant.save();
+  await newSession.save();
 
-    res.send('Participant Added');
-
+  res.send('Session Added');
 });
-app.get('/participants',
-async (req, res) => {
 
-    const participants =
-    await Participant.find();
+app.post('/add-participant', async (req, res) => {
+  const newParticipant = new Participant(req.body);
 
-    res.json(participants);
+  await newParticipant.save();
 
+  res.send('Participant Added');
 });
+
+app.get('/participants', async (req, res) => {
+  const participants = await Participant.find();
+
+  res.json(participants);
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log('Server running on port 5000');
-});
-app.post('/add-session', async (req, res) => {
-
-    const newSession = new Session(req.body);
-
-    await newSession.save();
-
-    res.send('Session Added');
-
+  console.log(`Server running on port ${PORT}`);
 });
